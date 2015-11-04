700d0705f477c99dd38c8ac0067a20a0b02d85e5
Ginanjar Abdul Wahab <gin.bqs@gmail.com>
2015-11-04 17:37:56 +0700

    insert-mst_barang(gagal).sql = > database yang duplicate primary key belum insertkan sudahditambah/sql => database yang sudah di insert

---
+++ - database -/insert-mst_barang(gagal).sql
@@ -0,0 +1,42 @@
+insert into mst_barang values ('0203010200','Kendaraan Bermotor Angkutan Barang');
+insert into mst_barang values ('0203010201','Truck + Attachhment');
+insert into mst_barang values ('0203010202','Pick Up');
+insert into mst_barang values ('0203010203','Trailer');
+insert into mst_barang values ('0203010204','Semi Trailer');
+insert into mst_barang values ('0203010204','Lain-lain');
+
+insert into mst_barang values ('0203050100','Kapal Terbang');
+
+insert into mst_barang values ('0204020100','Perkakas Bengkel Konstruksi Logam');
+
+
+insert into mst_barang values ('0204020200','Perkakas Khusus (Special Tool)');
+insert into mst_barang values ('0204020201','Kunci Khusus Untuk Engine');
+insert into mst_barang values ('0204020202','Kunci Khusus Untuk Alat Besar Darat');
+insert into mst_barang values ('0204020203','Kunci Khusus Untuk Alat Apung');
+insert into mst_barang values ('0204020204','Kunci Khusus Chasis Angkut Darat');
+
+
+insert into mst_barang values ('0204030100','Alat Ukut Universal');
+insert into mst_barang values ('0312060100','Tugu/Tanda Batas');
+	insert into mst_barang values ('0312080600','Flood Light');
+insert into mst_barang values ('0312080501','Tungsten Halogen Flood Light');
+insert into mst_barang values ('0312080502','Sodium Hight Prssure Flood Light');
+insert into mst_barang values ('0312080503','Mercury Flood Light');
+insert into mst_barang values ('0312080504','Lain-lain');
+	insert into mst_barang values ('0413020100','Jembatan Negara/Nasional');
+
+insert into mst_barang values ('0414020100','Bngunan Waduk');
+insert into mst_barang values ('0414060100',' Waduk AirBersih/AirBaku');
+
+insert into mst_barang values ('0415040101','Pengawetan Kayu');
+insert into mst_barang values ('0415040101','Pengeringan Kayu');
+insert into mst_barang values ('0415040101','Pengerjaan Kayu');
+insert into mst_barang values ('0415040101','Pengkapuran');
+insert into mst_barang values ('0415040101','Pembuatan Batu Cetak');
+insert into mst_barang values ('0415040101','Pembuatan  Anggregate');
+insert into mst_barang values ('0415040101','Lain-lain');
+
+insert into mst_barang values ('0415050100','Pembangkit Listrik Tenaga Air (PLTA)');
+
+insert into mst_barang values ('0519020100','Tanaman Perkebunan');
