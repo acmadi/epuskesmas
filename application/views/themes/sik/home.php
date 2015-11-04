cd8eaae5763186febfe979c3558b186884ecc6a0
Viktor <xu.redzonez@gmail.com>
2015-11-04 17:58:49 +0700

    update sidebar, navbar dan css

--- application/views/themes/sik/home.php
+++ application/views/themes/sik/home.php
@@ -58,7 +58,7 @@
           <span class="logo-lg"><b>Manajemen</b> Organisasi</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
+        <nav class="navbar" role="navigation">
           <!-- Sidebar toggle button-->
           <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
