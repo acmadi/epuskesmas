0aa105cb6a4688dc9d66c651ab6749f3e5cf1793
Viktor <xu.redzonez@gmail.com>
2015-11-06 17:18:58 +0700

    index master_sts

--- application/controllers/keuangan/master_sts.php
+++ application/controllers/keuangan/master_sts.php
@@ -5,6 +5,11 @@ class Master_sts extends CI_Controller {
 		parent::__construct();
 		$this->load->model('keuangan/sts_model');
 	}
+
+	function index(){
+		header("location:master_sts/anggaran");
+	}
+
 	function api_data(){
 		$data['ambildata'] = $this->sts_model->get_data();
 		foreach($data['ambildata'] as $d){
@@ -14,7 +19,7 @@ class Master_sts extends CI_Controller {
 		}
 	}
 	function anggaran(){
-		$this->authentication->verify('master_sts','edit');
+		$this->authentication->verify('keuangan','edit');
 		$data['title_group'] = "Anggaran";
 		$data['title_form'] = "Master Data - Keuangan";
 		$data['ambildata'] = $this->sts_model->get_data();
@@ -24,15 +29,15 @@ class Master_sts extends CI_Controller {
 		$this->template->show($data,"home");
 	}
 	function anggaran_add(){
-		$this->authentication->verify('master_sts','add');
+		$this->authentication->verify('keuangan','add');
 		$this->sts_model->add_anggaran();				
 	}
 	function anggaran_update(){
-		$this->authentication->verify('master_sts','edit');
+		$this->authentication->verify('keuangan','edit');
 		$this->sts_model->update_anggaran();				
 	}
 	function anggaran_delete(){
-		$this->authentication->verify('master_sts','del');
+		$this->authentication->verify('keuangan','del');
 		$this->sts_model->delete_anggaran();				
 	}
 
