9f8edee38103fc6b890659c190346e95d5b54650
Ginanjar Abdul Wahab <gin.bqs@gmail.com>
2015-11-27 09:09:38 +0700

    kib-A-F

--- - database -/view/kibA-F.sql
+++ - database -/view/kibA-F.sql
@@ -11,10 +11,6 @@ MySQL - 5.6.26 : Database - epus_org_3172100
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`epus_org_3172100` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `epus_org_3172100`;
-
 /*Table structure for table `get_all_inventaris` */
 
 DROP TABLE IF EXISTS `get_all_inventaris`;
